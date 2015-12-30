13698027f8aaae481d5d184bf0f80ad41cb03f3a
Aleksi Monaco <aleksi.monaco@beddit.com>
2015-12-30 11:21:30 +0200

    Create abstract Mapper class which is extended

--- classes/mappers.php
+++ classes/mappers.php
@@ -1,23 +1,55 @@
 <?php
 
-class SummonerSearchResultMapper {
+/**
+ * Mapper
+ *
+ */
+abstract class Mapper {
+	abstract public function mapToSingleModel($values);
+}
+
+/**
+ * Summoner Mapper
+ *
+ */
+class SummonerMapper extends Mapper {
 
 	public function __construct() {}
 
-	public function mapSingleSearchResult($result) {
-		if (sizeof($result) === 1) {
-			$resultAttributes = reset($result);
+	/**
+	 * 
+	 * Map array values to a Summoner model
+	 *
+	 * @param array $valeus Values that where returned from the API
+	 * @return Summoner $summoner mapped model for summoner data
+	 */
+	public function mapToSingleModel($values) {
 
-			$summoner = new Summoner($resultAttributes["id"],
-										$resultAttributes["name"], 
-										$resultAttributes["profileIconId"], 
-										$resultAttributes["revisionDate"], 
-										$resultAttributes["summonerLevel"]);
+		$summoner = new Summoner(
+			(int) 		$values["id"],
+			(string)	$values["name"], 
+			(int) 		$values["profileIconId"], 
+			(int)		$values["revisionDate"], 
+			(int)		$values["summonerLevel"]
+		);
 
-			return $summoner;
-		} else {
-			throw new Exception('Found multiple summoners');
-		}
+		return $summoner;
+	}
+}
+
+class MatchMapper {
+
+	public function __construct() {}
+
+	/**
+	 * 
+	 * Map array values to a single Match model
+	 *
+	 * @param array $values Attributes that where returned from the API
+	 * @return Match $match mapped model for single match data
+	 */
+	public function mapToSingleModel($values) {
+
 	}
 }
 
