6b4538a1802f95f364fa3b6c0039d585e89ee6a0
Aleksi Monaco <aleksi.monaco@hotmail.com>
2016-02-15 18:37:54 +0200

    Add date service and add method to convert timestamps to human format

--- app/js/directives.js
+++ app/js/directives.js
@@ -1,9 +1,12 @@
-lolApp.directive("recentMatch", function() {
+lolApp.directive("recentMatch", ["dateService", function(dateService) {
   return {
     restrict: 'E',
     scope: {
       match: "="
     },
-    templateUrl: "http://localhost/lol-match-history/app/js/templates/matchInfo.html"
+    templateUrl: "http://localhost/lol-match-history/app/js/templates/matchInfo.html",
+    link: function(scope, element, attrs) {
+      scope.match.since = dateService.getSince(scope.match.createDate);
+    }
   };
-});
+}]);
