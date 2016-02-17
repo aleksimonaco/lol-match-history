0e9f20af0f67cc129f46ada416c9e29f46dfda7c
Aleksi Monaco <aleksi.monaco@hotmail.com>
2016-02-17 18:06:54 +0200

    Config app to use ngRoute, default route is search page

--- app/js/app.js
+++ app/js/app.js
@@ -1,4 +1,4 @@
-var lolApp = angular.module('lolApp', ['angular-loading-bar', 'ngAnimate']);
+var lolApp = angular.module('lolApp', ['angular-loading-bar', 'ngAnimate', 'ngRoute']);
 
 lolApp.config(function($interpolateProvider) {
   $interpolateProvider.startSymbol('/*');
@@ -8,3 +8,16 @@ lolApp.config(function($interpolateProvider) {
 lolApp.config(['cfpLoadingBarProvider', function(cfpLoadingBarProvider) {
   cfpLoadingBarProvider.includeSpinner = false;
 }]);
+
+lolApp.config(['$routeProvider',
+  function($routeProvider) {
+    $routeProvider.
+    when('/', {
+        templateUrl: 'http://localhost/lol-match-history/app/js/templates/search.html',
+        controller: 'searchController'
+      }).
+      otherwise({
+        redirectTo: '/'
+      });
+  }
+]);
